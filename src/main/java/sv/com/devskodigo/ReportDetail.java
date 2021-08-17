e3b85d997215552ad104eca895e439c7fa1cb0ea
Irvin Torres <irvinrx1996@hotmail.com>
2021-08-17 14:54:40 -0600

    Classes mapped as shown on class diagram.

--- src/main/java/sv/com/devskodigo/ReportDetail.java
+++ src/main/java/sv/com/devskodigo/ReportDetail.java
@@ -3,11 +3,29 @@ package sv.com.devskodigo;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 public class ReportDetail implements DataOperations {
 
+    //TODO: should have a custom constructor?
     @Getter
     @Setter
     private static int x;
+    @Getter
+    @Setter
+    private int detailId;
+    @Getter
+    @Setter
+    private Date detailDateTime;
+    @Getter
+    @Setter
+    private String detailClassification;
+    @Getter
+    @Setter
+    private String detailDescription;
+    @Getter
+    @Setter
+    private ReportSummary reportSummary;
 
     @Override
     public void readDataset(){
--- src/main/java/sv/com/devskodigo/ReportSummary.java
+++ src/main/java/sv/com/devskodigo/ReportSummary.java
@@ -3,11 +3,34 @@ package sv.com.devskodigo;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+import java.util.List;
+
 public class ReportSummary implements DataOperations {
 
+    //TODO: should have a custom constructor?
     @Getter
     @Setter
     private static int x;
+
+    @Getter
+    @Setter
+    private int reportId;
+    @Getter
+    @Setter
+    private Date reportDateTime;
+    @Getter
+    @Setter
+    private int flightId;
+    @Getter
+    @Setter
+    private int userId;
+    @Getter
+    @Setter
+    private String reportStatus;
+    @Getter
+    @Setter
+    private List<ReportDetail> reportDetails;
 
     @Override
     public void readDataset(){
