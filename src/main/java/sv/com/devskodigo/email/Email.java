5257923a36be5d51c803dd7c718fab86514e9cb9
Irvin Torres <irvinrx1996@hotmail.com>
2021-08-19 18:52:55 -0600

    Build an email structure, with any numbers of recipients and multi parts

---
+++ src/main/java/sv/com/devskodigo/email/Email.java
@@ -0,0 +1,54 @@
+package sv.com.devskodigo.email;
+
+import lombok.Getter;
+import lombok.NonNull;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.List;
+
+public class Email {
+    private Address[] to;
+    private Multipart content;
+    private MimeMessage message;
+    public Email(Session session) {
+        message = new MimeMessage(session);
+        content = new MimeMultipart();
+    }
+
+    public void setTo( List<String> emailAddressList) throws MessagingException {
+        to = new Address[emailAddressList.size()];
+        for (int i = 0; i < emailAddressList.size(); i++) {
+            to[i] = new InternetAddress(emailAddressList.get(i));
+        }
+        message.addRecipients(Message.RecipientType.TO, to);
+    }
+    public void setFrom(String from) throws MessagingException{
+        message.setFrom(new InternetAddress(from));
+    }
+
+    public void setSubject(String subject)throws MessagingException {
+        message.setSubject(subject);
+    }
+
+    public void  addTextMessage(String text) throws MessagingException{
+        BodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setText(text);
+        content.addBodyPart(messageBodyPart);
+    }
+
+    public void  addAttachFile(String filename) throws MessagingException{
+        BodyPart messageBodyPart = new MimeBodyPart();
+        DataSource source = new FileDataSource(filename);
+        messageBodyPart.setDataHandler(new DataHandler(source));
+        messageBodyPart.setFileName(filename);
+        content.addBodyPart(messageBodyPart);
+    }
+    public MimeMessage getMessage() throws MessagingException{
+        message.setContent(content);
+        return message;
+    }
+}
